97cbe8ae66777e665f5f3c1e2f643ffdba2c730a
ts-firmaidraet <ts@firmaidraet.dk>
2016-04-12 14:02:11 +0200

    initial commit

---
+++ Test1.Logic/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1.Logic
+{
+    public class MyFirstClass
+    {
+        public int MyProperty { get; set; }
+
+        public string getString()
+        {
+            return "Dette er strengens indhold...5";
+        }
+    }
+}
---
+++ test1/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test1.Logic;
+
+namespace test1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            MyFirstClass cls = new MyFirstClass();
+            ViewBag.Stringresult = cls.getString();
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
